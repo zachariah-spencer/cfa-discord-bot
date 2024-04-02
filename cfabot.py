0b42b7d5439c2edf07523fe5ff669eb280c16abb
Zachariah <spencerzachariah@gmail.com>
2024-04-02 08:29:39 -0500

    Begin implementation of send_time_off_reminder()

--- cfabot.py
+++ cfabot.py
@@ -1,4 +1,4 @@
-import discord
+import discord, requests, datetime, asyncio
 
 # Set intents
 intents = discord.Intents.default()
@@ -6,6 +6,8 @@ intents.message_content = True
 
 # Define a discord Client object
 client = discord.Client(intents=intents)
+
+
 
 
 # A dictionary to simplify getting the role id's for any particular discord role
@@ -25,11 +27,12 @@ role_ids = {
 
 }
 
-
 # A dictionary to simplify getting the channel id's for any particular discord channel
 channel_ids = {
 
     "Shout Outs": 1212541612681338890,
+    "News And Updates": 1212541980895092767,
+    'Schedule': 1212541482507051100
 
 
 }
@@ -37,7 +40,20 @@ channel_ids = {
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    await send_time_off_reminder()
+    print(datetime.datetime.now().strftime("%H:%M UTC %a"))
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    #FIXME: Contact Fourth regarding External Customer Canonical ID's in HTTP requests. See about getting one or finding out what ours is.
+    if message.content.startswith('$hs'):
+        await message.channel.send('HotSchedules RESTful API HTTP Request')
+        #r = requests.get('https://httpbin.org/get')
+        #data = r.json()
+        #await message.channel.send(data)
 
 @client.event
 async def on_member_update(before, after):
@@ -66,6 +82,26 @@ async def handle_shout_outs(before, after):
 
         elif new_role.id == role_ids['Senior Team Member']:
             await channel.send('Everyone give a huge shout out to ' + after.display_name + ' on reaching Senior Team Member! :fire: :muscle:')
+
+#FIXME: Send time off cutoff reminder on Sundays and Mondays at a specific time each week
+async def send_time_off_reminder():
+    #FIXME Needs to set a flag message_sent to false until the minute for the message being sent has passed to avoid duplicate messages
+    #FIXME Needs to allow for setting a specific start time/date rather than beginning timer sequence on the time/date the bot script is ran.
+    message_sent = false
+    
+    while not message_sent:
+        now = datetime.datetime.now()
+
+        then = now + datetime.timedelta(days=7)
+        then = now.replace(hour=8, minute=25)
+
+        print((then - now).total_seconds)
+        wait_time = (then - now).total_seconds()
+        await asyncio.sleep(wait_time)
+        
+        channel = client.get_channel(channel_ids['Schedule'])
+
+        await channel.send('Remember to put in any scheduling requests you have for the upcoming week as the cutoff is at 3pm today!')
 
 # Run script via a discord bot, passing in the bot's token as the argument
 client.run('MTIyMzM2OTIyNjUxODAwNzg2OA.Gfby-v.sKqPTJUVHc72y10ggSSbkdryiFCcucMebyF12w')
